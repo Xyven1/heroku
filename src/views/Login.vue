6f0a179d86518f87913693ef50a33aab6e8a2a86
XyvenQ <maritimus987@gmail.com>
2020-09-05 17:01:12 -0700

    username should now behave as expected

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -11,11 +11,11 @@
               <v-list-item-subtitle>{{$store.state.profile.getEmail()}}</v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
-        </v-card>
-        <v-list-item>
-            <v-text-field label="Public Username" v-model="username"/>
+          <v-list-item>
+            <v-text-field label="Public Username" counter="20" v-model="username" :error-messages="error" :success-messages="success" :rules="usernameRules" @input="error=null; success=null"/>
             <v-btn @click="submit">Submit</v-btn>
           </v-list-item>
+        </v-card>
         <sign-in-button ref="SignIn" class="ma-2" color="primary" :dark="true" :onSignIn="signIn" :onSignOut="signOut"/>
       </v-col>
     </v-row>
@@ -27,7 +27,15 @@ import SignInButton from '../components/SignInButton.vue';
 export default {
   data (){
     return {
-      username: null
+      username: '',
+      success: null,
+      error: null,
+      usernameRules: [
+        value=>value == '' || value.length>2 || 'Username must at least 3 characters',
+        value=>value == '' || value.length<21 || 'Maximum 20 characters',
+        value=>value == '' || /^[a-zA-Z]/g.test(value) || 'Username must begin with a letter',
+        value=>value == '' || /^[a-zA-Z][a-zA-Z0-9.-]*$/g.test(value) || 'Username can only contain letters, numbers, -, and .',
+      ],
     }
   },
   components: {
@@ -36,23 +44,34 @@ export default {
   computed: {
   },
   methods: {
+    async getUsername(){
+      var vm = this
+      await vm.$auth.then(async auth => 
+        await vm.$axios.get('/database/user', {params: {idtoken: auth.currentUser.get().getAuthResponse().id_token}}).then(res=>
+          vm.username = res.data.username
+        )
+      )
+    },
     signIn(){
-      // this.$router.push(this.$route.query.redirect || '/')
+      this.getUsername()
     },
     signOut(){
     },
     async submit(){
       var vm = this
+      if(vm.username == '' || vm.username.length<3 || vm.username.length>20 || !/^[a-zA-Z][a-zA-Z0-9.-]*$/g.test(vm.username))
+        return
       await vm.$auth.then(async auth =>{
         vm.$axios.post('/database/user', {
           idtoken: auth.currentUser.get().getAuthResponse().id_token,
           username: vm.username
         }).then(res=>{
           if(res=="Success")
-            alert("It worked")
-          else{
-            console.log(res)
-          }
+            vm.success=`Your username is now ${vm.username}`
+          else if (res.data.code == "23505")
+            vm.error=`The username ${vm.username} is already taken`
+          else 
+          vm.error="Something went wrong..."
         })
       })
     },
@@ -65,10 +84,8 @@ export default {
         this.$store.commit('signIn')
         this.$router.push(this.$route.query.redirect)
       }
-      await vm.$axios.get('/database/user', {params: {idtoken: auth.currentUser.get().getAuthResponse().id_token}}).then((res)=>{
-        vm.username = res.data.username
-      })
     }).catch((e)=>console.log(e))
+    await vm.getUsername()
   }
 }
 </script>
